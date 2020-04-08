f4f9a4d1bfc231cb26b6d5625e4bcbad1061d92c
Taylor Neely <tneely417@gmail.com>
2020-04-07 19:20:25 -0700

    Initial commit

---
+++ bin/infrastructure.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { InfrastructureStack } from '../lib/infrastructure-stack';
+
+const app = new cdk.App();
+new InfrastructureStack(app, 'InfrastructureStack');
