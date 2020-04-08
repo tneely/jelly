e400c7c48a6ede22b1ff010f2f8abd0cde2680ca
Taylor Neely <tneely417@gmail.com>
2020-04-07 20:54:42 -0700

    Now with API Gateway

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda-nodejs";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -8,6 +9,10 @@ export class InfrastructureStack extends cdk.Stack {
     const hello = new lambda.NodejsFunction(this, "HelloFunction", {
       entry: "lambda/hello.ts",
       handler: "handler",
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
     });
   }
 }
