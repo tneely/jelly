3ada8c7fe0e9d6a76eef9d7ebfd12bf2b77adeb6
Taylor Neely <tneely417@gmail.com>
2020-08-20 22:12:00 -0700

    headers take 100

--- lib/constructs/edge-function.ts
+++ lib/constructs/edge-function.ts
@@ -3,31 +3,34 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as iam from "@aws-cdk/aws-iam";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 
-export interface EdgeFunctionProps extends Omit<lambda.FunctionProps, "environment"> {
+export interface EdgeFunctionProps extends Omit<lambda.FunctionProps, "environment" | "role"> {
   /**
    * The type of event in response to which should the function be invoked
    */
   eventType: cloudfront.LambdaEdgeEventType;
 }
 
-export class EdgeFunction extends lambda.Function implements cloudfront.EdgeLambda {
+export class EdgeFunction extends cdk.Construct implements cloudfront.EdgeLambda {
   public readonly functionVersion: lambda.IVersion;
   public readonly eventType: cloudfront.LambdaEdgeEventType;
 
   constructor(scope: cdk.Construct, id: string, props: EdgeFunctionProps) {
-    super(scope, id, props);
+    super(scope, id);
 
-    this.addToRolePolicy(
-      new iam.PolicyStatement({
-        actions: ["sts:AssumeRole"],
-        principals: [
+    const handler = new lambda.Function(this, "Resource", {
+      ...props,
+      role: new iam.Role(scope, "EdgeRole", {
+        assumedBy: new iam.CompositePrincipal(
           new iam.ServicePrincipal("lambda.amazonaws.com"),
-          new iam.ServicePrincipal("edgelambda.amazonaws.com"),
+          new iam.ServicePrincipal("edgelambda.amazonaws.com")
+        ),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
         ],
-      })
-    );
+      }),
+    });
 
-    this.functionVersion = this.currentVersion;
+    this.functionVersion = handler.currentVersion;
     this.eventType = props.eventType;
   }
 }
